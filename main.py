1bc529b8574dd6d8aa73b6f98eda1753b6f8c824
Emeka Allison <emekaallison4@gmail.com>
2021-05-29 11:08:09 +0200

    Added comments and instructions in README.md

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ app = FastAPI()
 
 keyPair = {}
 
+# Generates timestamps
 def generateTimestamp():
     return datetime.now()
 
+# Generates unique Id
 def generateUUID():
     return uuid.uuid1()
 
 @app.get("/")
 def generate():
+    '''
+    Generates a new uuid and pairs it with the time it was created
+    '''
     timestamp = generateTimestamp()
     uniqueId = generateUUID()
     keyPair[timestamp] = uniqueId
